24bdc7717b7f0537d59888f104043f4ae22781f8
skydrige <klokeshklokesh42376@gmail.com>
2024-03-23 20:14:45 +0530

    Techstack Changes

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,12 +1,10 @@
 /* eslint-disable */
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
   DiPython,
   DiGit,
   DiJava,
@@ -16,22 +14,14 @@ import {
   SiFirebase,
   SiTypescript,
   SiSolidity,
-  SiPostgresql,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/*<Col xs={4} md={2} className="tech-icons">*/}
-      {/*  <CgCPlusPlus />*/}
-      {/*</Col>*/}
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      {/*<Col xs={4} md={2} className="tech-icons">*/}
-      {/*  <TbBrandGolang />*/}
-      {/*</Col>*/}
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
@@ -40,9 +30,6 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSolidity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiTypescript />
@@ -56,9 +43,9 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiMongodb />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
+      {/*<Col xs={4} md={2} className="tech-icons">*/}
+      {/*  <SiPostgresql />*/}
+      {/*</Col>*/}
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
